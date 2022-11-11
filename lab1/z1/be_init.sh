8fb0284a516965f64e356306aab861e1f9ea7b93
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2022-11-11 21:44:45 +0100

    add db initialization

--- lab1/z1/be_init.sh
+++ lab1/z1/be_init.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/sh
 
 if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
-  echo "Usage: $0 DB URL" >&2
+  echo "Usage: $0 DB server ip address" >&2
   exit 1
 fi
 
-# spring.datasource.url
-export SPRING_DATASOURCE_URL="$1"
-export SPRING_PROFILES_ACTIVE=mysql,spring-data-jpa
-
+# Get Packages
 apt update -y
 apt install -y git openjdk-11-jdk
+
+# Get petclinic rest api
 git clone https://github.com/spring-petclinic/spring-petclinic-rest.git
 cd spring-petclinic-rest || exit
 
+# Set spring DB properties
+export SPRING_DATASOURCE_URL="jdbc:mysql://$1:3306/petclinic?useUnicode=true"
+export SPRING_PROFILES_ACTIVE=mysql,spring-data-jpa
+
+# Start petclinic rest api
 ./mvnw spring-boot:run
--- lab1/z1/db_init.sh
+++ lab1/z1/db_init.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-apt update -y
-apt install -y mysql-server
+DB_SCRIPTS_URI="https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql"
 
+# Get packages
+apt update -y
+apt install -y mysql-server wget
+
+# Configure and start mysql server
 sed 's/\(^bind-address\s*=\).*$/\1 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf -i
 service mysql start
 
-mysql -e "CREATE DATABASE petclinic"
+# Initialize database
 mysql -e "CREATE USER 'pc'@'%' IDENTIFIED BY 'petclinic'; GRANT ALL PRIVILEGES ON *.* TO 'pc'@'%' WITH GRANT OPTION;"
+wget "$DB_SCRIPTS_URI/initDB.sql" && mysql -f <"initDB.sql"
+wget "$DB_SCRIPTS_URI/populateDB.sql" && mysql petclinic -f <"populateDB.sql"
