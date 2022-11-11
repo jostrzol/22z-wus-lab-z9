e3c27dd3abf647cef7e80dd054609e0aa02c3d9d
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2022-11-12 00:30:17 +0100

    FE BE integration

--- lab1/z1/fe_init.sh
+++ lab1/z1/fe_init.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/sh
 
+if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
+  echo "Usage: $0 BE URL" >&2
+  exit 1
+fi
+
+BE_URL="$!"
+
 apt update -y
-apt install -y git curl sudo nginx
+apt install -y git curl
 
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# Install n node manager
+curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
+bash n lts
+npm install -g n
+n 12.11.1
 
-# npm uninstall -g angular-cli @angular/cli
-# npm cache clean
-npm install -g @angular/cli@11.2.11
+npm uninstall -g angular-cli @angular/cli
+npm cache clean --force
+echo N | npm install -g @angular/cli@11.2.11
 git clone https://github.com/spring-petclinic/spring-petclinic-angular.git
 cd spring-petclinic-angular || exit
 rm package-lock.json
 npm config set legacy-peer-deps true
-npm install --save-dev @angular/cli@11.2.11
+echo N | npm install --save-dev @angular/cli@11.2.11
 npm install
 
 echo N | ng analytics off
 
 # TODO configure deployment config (e.g. nginx)
-ng serve
+
+sed -i "s/localhost/${BE_URL}/g" src/environments/environment.ts
+sed -i "s/localhost/${BE_URL}/g" src/environments/environment.prod.ts
+
+ng serve --host 0.0.0.0
 
 
 
